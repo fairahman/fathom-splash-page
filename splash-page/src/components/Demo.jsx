529d9388fcc475c76cea1d9cb015eb15ac386645
frahman <faisalrahman3141@gmail.com>
2023-06-08 11:19:21 -0400

    added app dashboard screenshot to the page

--- splash-page/src/components/Demo.jsx
+++ splash-page/src/components/Demo.jsx
@@ -2,12 +2,12 @@
 import gif from '../assets/fathom_demo_quick.gif';
 export default function Demo() {
   return (
-    <div className=" bg-demoColor hero min-h-screen bg-base-200">
+    <div className=" bg-demoColor text-white hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
     <img src={gif} className="max-w-4xl rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="text-5xl font-bold">Stats you need</h1>
+      <p className="py-6 text-2xl" >With constant fetches and server-side rendering, Fathom gives you a wealth of metrics at unbelievable speed! </p>
       
     </div>
   </div>
